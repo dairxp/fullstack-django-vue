b35ae9bba40cb6e6a1414d1d9ed6f7aa766a12dd
Aldair Andrade <harperafterhours@gmail.com>
2026-04-27 23:53:43 -0500

    feat: primera implementacion modelo recetas

--- backend/recetas/models.py
+++ backend/recetas/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
-class Receta(models, Model):
-    nombre = models.CharField(max_length==100, null=False)
+class Receta(models.Model):
+    nombre = models.CharField(max_length=100, null=False)
     slug = AutoSlugField(populate_from= 'nombre', max_length=100)
-    tiempo =models.CharField(maz_length=100,null=False)
+    tiempo =models.CharField(max_length=100,null=True)
+    foto =models.CharField(max_length=100,null=True)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return self.nombre
