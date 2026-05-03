395fc1179b95f449bfa3f309765901c2cb263f47
Aldair Andrade <harperafterhours@gmail.com>
2026-05-02 22:17:58 -0500

    feart:asvanze  recetas. models y views

--- backend/recetas/models.py
+++ backend/recetas/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from autoslug import AutoSlugField
+from categorias.models import Categoria
 
 # Create your models here.
 class Receta(models.Model):
+    categorias = models.ForeignKey(Categoria, models.DO_NOTHING) # , default=1
     nombre = models.CharField(max_length=100, null=False)
     slug = AutoSlugField(populate_from= 'nombre', max_length=100)
     tiempo =models.CharField(max_length=100,null=True)
     foto =models.CharField(max_length=100,null=True)
     descripcion = models.TextField()
     fecha = models.DateTimeField(auto_now=True)
-    
+
 
     def __str__(self):
         return self.nombre
 
     class Meta:
-        db_table= 'categorias'
-        verbose_name= 'Categoria'
-        verbose_name_plural = 'Categorias'
+        db_table= 'recetas'
+        verbose_name= 'Receta'
+        verbose_name_plural = 'Recetas'
--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http.response import JsonResponse
+from http import HTTPStatus
+from django.http import Http404
+from django.utils.text import slugify
+from .serializers import *
+from .models import *
 
 # Create your views here.
-class Clase1(ApiView):
-    pass
+class Clase1(APIView):
+    
+    def get(self, request):
+        data =Receta.objects.order_by('-id').all()
+        datos_json= RecetaSerilalizer(data, many=True)
+        return JsonResponse({"data":datos_json.data})
