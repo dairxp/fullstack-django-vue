aa094cf828c017a5b13a14934f596780d073a758
Aldair Andrade <harperafterhours@gmail.com>
2026-06-26 12:07:34 -0500

    Validad nombre de receta mas upload de fotos

--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -1,15 +1,16 @@
-from functools import _NOT_FOUND
+#from functools import _NOT_FOUND
 from rest_framework.views import APIView
 from django.http.response import JsonResponse
 from http import HTTPStatus
 from django.http import Http404
 from django.utils.text import slugify
-from .serializers import *
-from .models import *
+from .serializers import RecetaSerializer
+from .models import Receta, Categoria
 from django.utils.dateformat import DateFormat
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 class Clase1(APIView):
@@ -29,13 +30,35 @@ class Clase1(APIView):
         if request.data.get("categoria_id")==None or not request.data["categoria_id"]:
             return JsonResponse({"estado":"error", "mensaje":"El campo categoria_id es obligatoria"}, status=HTTPStatus.BAD_REQUEST)
 
+        #validar no exista la categoriID
+        try:
+            #categoria =Categoria.objects.filter(pk=request.data["categoria_id"].get())
+            categoria = Categoria.objects.get(pk=request.data.get("categoria_id"))
+        except Categoria.DoesNotExist:
+            return JsonResponse({"estado":"error", "mensaje":"La categoria_id no existe en al base de datos"}, status=HTTPStatus.BAD_REQUEST)
+        # En lugar de try/except, usamos un simple IF
+        # if not Categoria.objects.filter(pk=request.data.get("categoria_id")).exists():
+        #     return JsonResponse({"estado":"error", "mensaje":"La categoria_id no existe"}, status=HTTPStatus.BAD_REQUEST)
+
         #select *from recetas where nombre=request.data.get("nombre")
         #validamos nombre de receta este disponible
         if Receta.objects.filter(nombre=request.data.get("nombre")).exists():
             return JsonResponse({"estado":"error", "mensaje":f"El nombre {request.data['nombre']} no esta disponible"}, status=HTTPStatus.BAD_REQUEST)
 
+        fs =FileSystemStorage()
         try:
-            Receta.objects.create(nombre=request.data["nombre"], tiempo=request.data.get("tiempo"), descripcion=request.data["descripcion"], categorias_id=request.data.get("categoria_id"), fecha=datetime.now(), foto="SSS")
+            foto = f"{datetime.timestamp(datetime.now())}{os.path.splitext(str(request.FILES['foto']))[1]}"
+        except Exception as e:
+            return JsonResponse({"estado":"error", "mensaje":"Debe adjuntar una foto para la receta"}, status=HTTPStatus.BAD_REQUEST)
+
+        try:
+            fs.save(f"recetas/{foto}", request.FILES['foto'])
+            fs.url(request.FILES['foto'])
+        except Exception as e:
+            return JsonResponse({"estado":"error", "mensaje":"Se produjo un error al subir un archivo"}, status=HTTPStatus.BAD_REQUEST)
+
+        try:
+            Receta.objects.create(nombre=request.data["nombre"], tiempo=request.data.get("tiempo"), descripcion=request.data["descripcion"], categorias_id=request.data.get("categoria_id"), fecha=datetime.now(), foto=foto)
 
              ### Receta.objects.create(nombre=request.data["nombre"], tiempo=request.data.get("tiempo"), descripcion=request.data["descripcion"], categorias_id=request.data.get("categoria_id"), fecha=datetime.now(), foto="SSS")
             return JsonResponse({"estado":"OK", "mensaje": "Se crea el registro existosamente"}, status=HTTPStatus.CREATED)
@@ -49,5 +72,5 @@ class Clase2(APIView):
             data = Receta.objects.filter(id=id).get()
             return JsonResponse({"data":{"id":data.id,"nombre":data.nombre, "slug":data.slug, "tiempo":data.tiempo, "descripcion":data.descripcion, "fecha":DateFormat(data.fecha).format('d/m/Y'), "categoria_id":data.categorias_id, "categoria":data.categorias.nombre, "imagen":f"{os.getenv('BASE_URL')}uploads/recetas/{data.foto}"}}, status=HTTPStatus.OK)
 
-        except:
+        except Exception as e:
             return JsonResponse({"estado":"error", "mensaje":"Recursos no disponible"}, status=HTTPStatus.NOT_FOUND)
